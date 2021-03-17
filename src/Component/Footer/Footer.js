b7f1dff26f485ebb85fff217c1c58b6fca69b20e
Nikki <nikki.satopdx@gmail.com>
2021-03-16 22:33:54 -0700

    finished footer

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -6,7 +6,6 @@ import SkipNextIcon from '@material-ui/icons/SkipNext';
 import ShuffleIcon from '@material-ui/icons/Shuffle';
 import RepeatIcon from '@material-ui/icons/Repeat';
 import VolumeDownIcon from '@material-ui/icons/VolumeDown';
-import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
 import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
 import { Grid, Slider } from '@material-ui/core';
 
@@ -14,7 +13,15 @@ function Footer() {
   return (
     <div className='footer'>
       <div className='footer__left'>
-        <p>Album and song details</p>
+        <img
+          className='footer__albumLogo'
+          src='https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Usher_-_Confessions_album_cover.jpg/220px-Usher_-_Confessions_album_cover.jpg'
+          alt=''
+        />
+        <div className='footer__songInfo'>
+          <h4>Yeah!</h4>
+          <p>Usher!</p>
+        </div>
       </div>
       <div className='footer__center'>
         <ShuffleIcon className='footer__green' />
@@ -24,7 +31,6 @@ function Footer() {
         <RepeatIcon className='footer__green' />
       </div>
       <div className='footer__right'>
-        {' '}
         <Grid container spacing={2}>
           <Grid item>
             <PlaylistPlayIcon />
